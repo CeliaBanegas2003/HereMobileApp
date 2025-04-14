40667e076b0522e0be1b42f9e9d6d44cccd91c2a
CeliaBanegas2003 <9104954@alumnos.ufv.es>
2025-04-14 13:32:44 +0200

    backend incluido

---
+++ hereapp-backend/src/test/java/com/example/hereapp_backend/HereappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hereapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
