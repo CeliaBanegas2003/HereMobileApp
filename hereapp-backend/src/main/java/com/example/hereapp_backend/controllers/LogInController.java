0d0967c575f4733b3967c4b138ee1108a6c92b75
CeliaBanegas2003 <9104954@alumnos.ufv.es>
2025-04-16 10:16:53 +0200

    Cambios realizados

--- hereapp-backend/src/main/java/com/example/hereapp_backend/controllers/LogInController.java
+++ hereapp-backend/src/main/java/com/example/hereapp_backend/controllers/LogInController.java
@@ -1,12 +1,12 @@
 package com.example.hereapp_backend.controllers;
 
-import com.example.hereapp_backend.Request.LogInRequest;
 import com.example.hereapp_backend.dataAccess.LogInBBDD;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*") // Permite peticiones de cualquier origen (útil para desarrollo)
 @RestController
 public class LogInController {
 
@@ -16,15 +16,12 @@ public class LogInController {
         this.logInBBDD = logInBBDD;
     }
 
-    @PostMapping("/tipo")
+    @PostMapping("/login")
     public ResponseEntity<String> loginTipo(@RequestBody Map<String, String> body) {
         String email = body.get("email");
         String contrasena = body.get("contrasena");
 
-
         String rol = logInBBDD.validarTipoUsuario(email, contrasena);
-
-
         if (rol == null) {
             return ResponseEntity.ok("NONE");
         } else {
